27daf34f1df59fd2f87c64a3a48f4c461aec275f
Zach Bensky <zachbensky@gmail.com>
2025-03-09 21:15:04 -0400

    Implemented user input onto the grid and cleaned up UI

--- app.js
+++ app.js
@@ -2,18 +2,49 @@
 import { solvePuzzle } from "./solver.js";
 import { generatePuzzle } from "./generate.js";
 
-let grid = document.querySelectorAll(".square");
+let gridArray = document.querySelectorAll(".square");
 let values = document.querySelectorAll("span");
+
+let focusIndex = 0;
 
 /**
  * fills grid with accurate id selectors
- * @param {NodeList} grid
  */
 const initializeGrid = () => {
-  for (let i = 0; i < grid.length; i++) {
-    grid[i].setAttribute("id", i);
+  for (let i = 0; i < gridArray.length; i++) {
+    gridArray[i].setAttribute("id", i);
+    gridArray[i].setAttribute("highlighted", 0);
     values[i].setAttribute("id", "value" + i);
+    values[i].setAttribute("set", 0);
+    // add event listener to grid square
+    gridArray[i].addEventListener("click", () => {
+      // highlight square and unhighlight previous square
+      gridArray[focusIndex].setAttribute("highlighted", 0);
+      gridArray[i].setAttribute("highlighted", 1);
+      focusIndex = i;
+    });
   }
+};
+
+/**
+ * enters key value into clicked square, assuming e.key is a valid number 1-9
+ * @param {Number} squareID
+ * @param {Event} e
+ */
+const enterNumber = (squareID, e) => {
+  // validate input and make sure square is not set (set == 1)
+  if (validNumber(e.key) && values[focusIndex].getAttribute("set") != 1) {
+    values[squareID].innerHTML = Number(e.key);
+  }
+};
+
+/**
+ * Returns true if s is a number between 1-9
+ * @param {String} s
+ */
+const validNumber = (s) => {
+  const validNums = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  return validNums.includes(s);
 };
 
 /**
@@ -22,19 +53,21 @@ const initializeGrid = () => {
 const clearValues = () => {
   values.forEach((val) => {
     val.innerHTML = "";
+    val.setAttribute("set", 0);
   });
 };
 
 /**
- * fills grid with the corresponding numbers in grid_nums
- * @param {Array<Array<Number>>} grid_nums
+ * fills grid with the corresponding numbers in gridNums
+ * @param {Array<Array<Number>>} gridNums
+ * @param {Boolean} isSet
  */
-const displayGrid = (grid_nums) => {
+const displayGrid = (gridNums, isSet) => {
   // collapse into 1D array
   let gridData = [];
-  for (let r = 0; r < grid_nums.length; r++) {
-    for (let c = 0; c < grid_nums[r].length; c++) {
-      gridData.push(grid_nums[r][c]);
+  for (let r = 0; r < gridNums.length; r++) {
+    for (let c = 0; c < gridNums[r].length; c++) {
+      gridData.push(gridNums[r][c]);
     }
   }
   // add to grid (if not 0) and clear values first
@@ -42,15 +75,25 @@ const displayGrid = (grid_nums) => {
   for (let i = 0; i < gridData.length; i++) {
     if (gridData[i] != 0) {
       values[i].innerHTML = gridData[i];
+      if (isSet) {
+        values[i].setAttribute("set", 1);
+      }
     }
   }
 };
 
-const generateButton = document.getElementById("generate");
-generateButton.addEventListener("click", () => {
-  const grid_nums = generatePuzzle(40); // arbitrary value
-  displayGrid(grid_nums);
+// Enter number on key press
+const grid = document.querySelector(".grid");
+grid.addEventListener("keydown", (e) => {
+  enterNumber(focusIndex, e);
 });
 
-initializeGrid(grid);
-console.log(grid);
+// Generate new grid
+const generateButton = document.getElementById("generate");
+generateButton.addEventListener("click", () => {
+  const gridNums = generatePuzzle(40); // arbitrary value
+  displayGrid(gridNums, true);
+});
+
+initializeGrid(gridArray);
+console.log(gridArray);
